6331986b4a91cfe40b8a5b813da174c849deeba4
Kevin <kevinity310@gmail.com>
2022-04-08 09:07:21 +0700

    Persiapan deploy

--- connection/db.js
+++ connection/db.js
@@ -1,11 +1,23 @@
+const { Pool } = require('pg')
 
-const {Pool} = require('pg')
+const isProduction = process.env.NODE_ENV === "production";
+let dbPool
 
-const dbPool = new Pool({
-    database: 'task_web_b33',
-    port: 5432,
-    user: 'postgres',
-    password: 'root'
-})
+if (isProduction) {
+    dbPool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
+} else {
+
+    dbPool = new Pool({
+        database: 'task_web_b33',
+        port: 5432,
+        user: 'postgres',
+        password: 'root'
+    })
+}
 
 module.exports = dbPool

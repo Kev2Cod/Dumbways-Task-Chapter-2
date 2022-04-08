5d963eb080921432c0bcd06722f1c930c4db9eb0
Kevin <kevinity310@gmail.com>
2022-04-08 12:51:33 +0700

    Merapikan code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const session = require('express-session')
 const flash = require('express-flash')
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = 5000
 
 const db = require('./connection/db')
 const upload = require('./middlewares/fileUpload')
@@ -29,9 +29,7 @@ app.use(session({
 }))
 
 
-
-
-// SHOW FETCH DATABASE
+// GET : INDEX
 app.get('/', function (req, res) {
 
     const query = `SELECT tb_projects.id as project_id, tb_users.id AS author_id, tb_users.name AS author_name, tb_projects.name AS project_name, start_date, end_date, description, technologies, image
@@ -44,7 +42,6 @@ app.get('/', function (req, res) {
             client.query(query, function (err, result) {
                 if (err) throw err // Kondisi untuk menampilkan error query
                 let data = result.rows
-                done()
 
                 data = data.map(function (item) {
                     return {
@@ -75,6 +72,7 @@ app.get('/', function (req, res) {
             client.query(queryAfterLogin, function (err, result) {
                 if (err) throw err // Kondisi untuk menampilkan error query
                 let data = result.rows
+                done
 
                 data = data.map(function (item) {
                     return {
@@ -92,68 +90,20 @@ app.get('/', function (req, res) {
                         isLogin: req.session.isLogin
                     }
                 })
-
                 res.render('index', { isLogin: req.session.isLogin, user: req.session.user, project: data })
             })
         }
     })
 })
 
-// GET: DELETE PROJECT
-app.get('/delete-project/:id', (req, res) => {
-    const id = req.params.id
-
-    const querySelect = `SELECT image FROM tb_projects WHERE tb_projects.id =${id};`
-
-    db.connect(function (err, client, done) {
-        if (err) throw err // Kondisi untuk menampilkan error koneksi database
-
-        client.query(querySelect, function (err, result) {
-            if (err) throw err // Kondisi untuk menampilkan error query
-            let data = result.rows[0].image
-
-            const removeImage = (filePath) => {
-                console.log('filePath: ', filePath)
-                console.log('dir name: ', __dirname)
-
-                filePath = path.join(__dirname, '/uploads', filePath)
-                fs.unlink(filePath, err => console.log(err))
-            }
-            removeImage(data)
-        })
-
-        const queryDelete = `DELETE FROM tb_projects WHERE tb_projects.id=${id};`
-        client.query(queryDelete, function (err, result) {
-            if (err) throw err // Kondisi untuk menampilkan error query
-            let data = result.rows
-            // done()
-            console.log(data)
-            res.redirect('/')
-        })
-    })
-
-})
-
-
-
-// GET: ADD PROJECT
-app.get('/add-project', function (req, res) {
-    if (!req.session.isLogin) {
-        req.flash('danger', 'Silahkan Login!!')
-        return res.redirect('/login')
-    }
-
-    res.render('add-project', { isLogin: req.session.isLogin, user: req.session.user })
-})
 
 
 // GET: REGISTER
 app.get('/register', (req, res) => {
-
     res.render('register')
 })
 
-// POST: REGISTER
+// POST: REGISTER   
 app.post('/register', (req, res) => {
     let { inputName, inputEmail, inputPassword } = req.body //! destruction
     const hashedPassword = bcrypt.hashSync(inputPassword, 10)
@@ -167,11 +117,9 @@ app.post('/register', (req, res) => {
 
         client.query(querySelect, function (err, result) {
             if (err) throw err
-            done()
 
             if (result.rows.length != 0) {
                 req.flash('danger', 'Email telah terdaftar!')
-                done()
                 return res.redirect('/register')
             } else {
                 client.query(queryInsert, function (err, result) { //! Error jika menambahkan email baru
@@ -186,9 +134,9 @@ app.post('/register', (req, res) => {
 })
 
 
+
 // GET: LOGIN
 app.get('/login', (req, res) => {
-
     res.render('login')
 })
 
@@ -210,8 +158,6 @@ app.post('/login', (req, res) => {
             }
 
             let dataUser = result.rows[0] //! data user ditampung di variabel baru
-
-
             let isMatch = bcrypt.compareSync(inputPassword, dataUser.password)
 
             if (isMatch) {
@@ -225,16 +171,16 @@ app.post('/login', (req, res) => {
                 console.log('Berhasil Login');
                 req.flash('success', 'Login Success')
                 res.redirect('/')
-
             } else {
                 console.log('Password Salah');
                 req.flash('warning', 'Password Salah')
                 res.redirect('/login')
             }
-
         })
     })
 })
+
+
 
 // GET: LOGOUT
 app.get('/logout', function (req, res) {
@@ -242,10 +188,7 @@ app.get('/logout', function (req, res) {
     res.redirect('/')
 })
 
-// GET: CONTACT
-app.get('/contact', function (req, res) {
-    res.render('contact')
-})
+
 
 // GET: DETAIL PROJECT
 app.get('/detail-project/:id', function (req, res) {
@@ -279,6 +222,17 @@ app.get('/detail-project/:id', function (req, res) {
             res.render('detail-project', { project: data })
         })
     })
+})
+
+
+
+// GET: ADD PROJECT
+app.get('/add-project', function (req, res) {
+    if (!req.session.isLogin) {
+        req.flash('danger', 'Silahkan Login!!')
+        return res.redirect('/login')
+    }
+    res.render('add-project',)
 })
 
 // POST: ADD PROJECT
@@ -315,7 +269,9 @@ app.post('/add-project', upload.single('inputImage'), function (req, res) {
     })
 })
 
-// GET DATA for UPDATE PROJECT
+
+
+// GET : EDIT PROJECT
 app.get('/edit-project/:id', (req, res) => {
     const id = req.params.id
 
@@ -346,6 +302,43 @@ app.get('/edit-project/:id', (req, res) => {
         })
     })
 })
+
+
+
+// GET: DELETE PROJECT
+app.get('/delete-project/:id', (req, res) => {
+    const id = req.params.id
+
+    const querySelect = `SELECT image FROM tb_projects WHERE tb_projects.id =${id};`
+
+    db.connect(function (err, client, done) {
+        if (err) throw err // Kondisi untuk menampilkan error koneksi database
+
+        client.query(querySelect, function (err, result) {
+            if (err) throw err // Kondisi untuk menampilkan error query
+            let data = result.rows[0].image
+
+            const removeImage = (filePath) => {
+                console.log('filePath: ', filePath)
+                console.log('dir name: ', __dirname)
+
+                filePath = path.join(__dirname, '/uploads', filePath)
+                fs.unlink(filePath, err => console.log(err))
+            }
+            removeImage(data)
+        })
+
+        const queryDelete = `DELETE FROM tb_projects WHERE tb_projects.id=${id};`
+        client.query(queryDelete, function (err, result) {
+            if (err) throw err // Kondisi untuk menampilkan error query
+            let data = result.rows
+            done()
+            console.log(data)
+            res.redirect('/')
+        })
+    })
+})
+
 
 // POST: UPDATE PROJECT
 app.post('/update-project/:id', upload.single('inputImage'), (req, res) => {
@@ -382,6 +375,14 @@ app.post('/update-project/:id', upload.single('inputImage'), (req, res) => {
         })
     })
 })
+
+
+
+// GET: CONTACT
+app.get('/contact', function (req, res) {
+    res.render('contact')
+})
+
 
 
 
